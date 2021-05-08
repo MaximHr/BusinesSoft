b4231fe9c6363c698765c9fa6bf43bd9e220c56e
Maxim <hristivi8383@gmail.com>
2021-05-08 20:35:02 +0300

    test

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,7 @@ const App = () => {
                     <SideBar sidebar={sidebar} setSidebar={setSidebar}/>
                     <Navbar sidebar={sidebar} setSidebar={setSidebar}/>
                     <Switch>
-                        <Route path='/home' exact>
+                        <Route path='/' exact>
                             <Home />
                         </Route>
                         <Route path='/about'>
